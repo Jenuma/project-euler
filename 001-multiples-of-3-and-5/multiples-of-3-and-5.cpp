1d586786b1f42d45f76c25c13ada8f7dc36c4b4d
Clifton Roberts <Jenuma@live.com>
2016-11-14 20:49:21 -0600

    Initial commit

---
+++ 001-multiples-of-3-and-5/multiples-of-3-and-5.cpp
@@ -0,0 +1,33 @@
+/*
+ * Project Euler Problem #001 - Multiples of 3 and 5
+ * If we list all the natural numbers below 10 that are multiples of
+ * 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ * Author: Clifton Roberts
+ * Date: 11 November 2016
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int LIMIT = 1000;
+
+	int sum = 0;
+	for (int i = 0; i < LIMIT; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	cout << sum << endl;
+
+	system("pause");
+	return 0;
+}
